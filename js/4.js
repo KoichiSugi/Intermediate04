a9d3846a9aa616d9da2ca58dfa6697307cc7ef84
KoichiSugi <43499213+KoichiSugi@users.noreply.github.com>
2021-05-23 23:07:36 +1000

    Add files via upload

---
+++ js/4.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function () {
+  /* Promise to make sure data loads */
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "https://myfxmarkets.com/1eb7daee5f804bd6b3698ba6a11aa41d/index.php/Jsontest",
+      method: "GET",
+      dataType: "JSON",
+      success: function (data) {
+        resolve(data);
+        console.log(data);
+        displayTable(data);
+      },
+      error: function (error) {
+        reject(error);
+        console.log(error);
+      },
+    });
+  });
+});
+
+function displayTable(data) {
+ var tableBody = "";
+ var odd_even = false;
+ $.each(data.data, function() {
+    var table_row = "";
+    $.each(this, function(k , v) {
+        table_row += "<td>"+v+"</td>";
+    });
+    tableBody += "<tr class=\""+( odd_even ? "odd" : "even")+"\">"+table_row+"</tr>";
+    odd_even = !odd_even;
+});
+$("#tbody").html(tableBody);
+}
